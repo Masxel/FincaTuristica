82b1ec40ce048a6b95e72e80f9384d920929071a
Johan <ferneydaza009@gmail.com>
2025-09-29 20:43:42 -0500

    Modificacion de tablas, nuevas tablas

--- script.sql
+++ script.sql
@@ -3,21 +3,14 @@ CREATE DATABASE db_fincaturistica;
 
 CREATE TABLE `db_fincaturistica`.`reserva` (
 	`id` INT AUTO_INCREMENT NOT NULL,
-    `fechareserva` DATE NOT NULL,
     `fechallegada` DATE NOT NULL,
     `fechasalida` DATE NOT NULL,
     `idcliente` int NOT NULL,
     `idhabitacion` int NOT NULL,
     `estadoreserva` int NOT NULL,
-    `metodopago` int NOT NULL,
     PRIMARY KEY(`id`)
 );
 
-CREATE TABLE `db_fincaturistica`.`metodopago` (
-    `id` INT AUTO_INCREMENT NOT NULL,
-    `descripcion` VARCHAR(45) NOT NULL,
-    PRIMARY KEY(`id`)
-);
 
 CREATE TABLE `db_fincaturistica`.`estadoreserva` (
     `id` INT AUTO_INCREMENT NOT NULL,
@@ -76,4 +69,36 @@ CREATE TABLE `db_fincaturistica`.`cliente` (
     PRIMARY KEY(`id`)
 );
 
+CREATE TABLE `db_fincaturistica`.`factura` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `fecha` DATE NOT NULL,
+    `total` int NOT NULL,
+    `idreserva` int NOT NULL,
+    `idmetodopago` int NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_factura__reserva` FOREIGN KEY (`idreserva`) REFERENCES `reserva`(`id`),
+    CONSTRAINT `fk_factura__metodo_pago` FOREIGN KEY (`idmetodopago`) REFERENCES `metodo_pago`(`id`)
+);
 
+CREATE TABLE `db_fincaturistica`.`metodo_pago` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `descripcion` VARCHAR(45) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+--Son zonas como piscinas, jacuzzis, canchas deportivas, salones de eventos, etc.
+CREATE TABLE `db_fincaturistica`.`zonas_entretenimiento` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `nombre` VARCHAR(45) NOT NULL,
+    `descripcion` VARCHAR(200) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+-- Eventos especiales, como bodas, cumpleaños, conferencias, etc.
+CREATE TABLE `db_fincaturistica`.`eventos` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `nombre` VARCHAR(45) NOT NULL,
+    `descripcion` VARCHAR(200) NOT NULL,
+    `fecha` DATE NOT NULL,
+    PRIMARY KEY(`id`)
+);
